cfbcd481441f83e4bc8a9eac9f52a75502e4f92e
Water261 <48581492+Water261@users.noreply.github.com>
2021-11-15 17:34:34 +1000

    Revert "Merge branch 'wally' into main"
    This reverts commit a6ea5dccfd3c2c2c9b080338b60860207178bc2b, reversing
    changes made to 1d46def63d19d7ddc069f074e977179eaed2b63e.

--- src/init.lua
+++ src/init.lua
@@ -8,14 +8,10 @@
 
 --]]
 
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Signal = require(ReplicatedStorage.Packages:WaitForChild("Signal"))
-
 local config = require(script.Configuration)
 local utils = require(script.Utilities)
 
 local holder = utils.constructFromProperties("Folder", config.defaults.holder)
-local teamChangeSignal = Signal.new()
 
 local nametagPlus = {}
 
@@ -151,7 +147,6 @@ local function linkTag(player: Player, tag: BillboardGui, groupId: number | nil)
 				nameLabel.TextStrokeTransparency = 1
 				nameLabel.TextStrokeColor3 = config.presets["colors"]["secondary"]
 			end
-			teamChangeSignal:Fire(player.TeamColor.Color)
 		end
 		updateTeamColor() -- Updates the team color preemptively just in case.
 		player:GetPropertyChangedSignal("Team"):Connect(updateTeamColor)
@@ -191,6 +186,5 @@ nametagPlus.addTag = addTag
 nametagPlus.getTag = getTag
 nametagPlus.changeTag = changeTag
 nametagPlus.linkTag = linkTag
-nametagPlus.teamChange = teamChangeSignal
 
 return nametagPlus
